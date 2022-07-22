16e88afda355ea61c246b973633f81c67655ec66
Santi de Nicolás <santidenicolas@gmail.com>
2022-07-22 04:32:40 -0300

    :fire: Remove all olds js and css files

--- src/js/checkout.js
+++
@@ -1,8 +0,0 @@
-const processing = document.getElementById('processing')
-const payScreen = document.getElementById('pay_screen')
-
-setTimeout(() => {
-  processing.classList.add('slide-up')
-  payScreen.classList.add('slide-in')
-  localStorage.clear()
-}, 2000)
